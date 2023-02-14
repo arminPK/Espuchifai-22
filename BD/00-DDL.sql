1baa3173e714141433071a6b07186a1bb398d7d8
arminPK <armin.mercadoet12d1@gmail.com>
2023-02-14 18:36:34 -0300

    actualizado

--- BD/00-DDL.sql
+++ BD/00-DDL.sql
@@ -1,46 +1,62 @@
 DROP DATABASE IF EXISTS Espuchifai;
+SELECT 'Creando bd' Estado;
 CREATE DATABASE Espuchifai;
-CREATE TABLE Espuchifai.Banda(
-    idBanda TINYINT UNSIGNED NOT NULL,
-    nombre VARCHAR(45)   NOT NULL,
-    fundacion YEAR NOT NULL,
-    PRIMARY KEY(idBanda),
-);
-CREATE TABLE Espuchifai.Album(
-    idAlbum INT UNSIGNED NOT NULL,
-    idBanda TINYINT UNSIGNED NOT NULL,
-    nombre VARCHAR(45) NOT NULL,
-    lanzamiento DATE NOT NULL,
-    PRIMARY KEY (idAlbum),
-    CONSTRAINT FK_Album_banda FOREIGN KEY (idBanda)
-        REFERENCES Espuchifai.banda (idBanda),
-    CONSTRAINT UQ_Album_nombre UNIQUE (nombre)
-);
-CREATE TABLE Espuchifai.Cancion (
-    idCancion INT UNSIGNED NOT NULL,
-    idAlbum MEDIUMINT UNSIGNED NOT NULL,
-    nombre VARCHAR(45) NOT NULL,
-    posicion TINYINT UNSIGNED NOT NULL,
-    PRIMARY KEY (idCancion),
-    CONSTRAINT FK_Cancion_Album FOREIGN KEY(idAlbum)
-        REFERENCES Espuchifai.Album (idAlbum),
-    CONSTRAINT UQ_Cancion_nombre UNIQUE (nombre)
-);
-CREATE TABLE Espuchifai.Reproduccion(
-    idCliente  SMALLINT UNSIGNED NOT NULL,
-    idCancion  INT UNSIGNED NOT NULL,
-    reproduccion DATETIME NOT NULL,
-    PRIMARY KEY (idCliente),
-    CONSTRAINT FK_Reproduccion_Cancion FOREIGN KEY (idCancion)
-        REFERENCES Espuchifai.Cancion (idCancion),
-    CONSTRAINT FK_Reproduccion_idCliente FOREIGN KEY  (idCliente)
-        REFERENCES Espuchifai.Cliente (idCliente)
-);
-CREATE TABLE Espuchifai.Cliente(
-    idCliente SMALLINT UNSIGNED NOT NULL,
-    nombre VARCHAR (45) NOT NULL,
-    apellido VARCHAR (45) NOT NULL,
-    email VARCHAR (45) NOT NULL,
-    contrasenia CHAR (64) NOT NULL,
-    PRIMARY KEY (idCliente)
-);
+
+CREATE TABLE
+    Espuchifai.Banda(
+        nombre VARCHAR(45) NOT NULL,
+        fundacion YEAR NOT NULL,
+        idBanda SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+        PRIMARY KEY(idBanda),
+        FULLTEXT (nombre)
+    );
+
+CREATE TABLE
+    Espuchifai.Usuario(
+        nombre VARCHAR (45) NOT NULL,
+        apellido VARCHAR (45) NOT NULL,
+        email VARCHAR (45) NOT NULL,
+        contrasenia CHAR (64) NOT NULL,
+        idUsuario SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+        PRIMARY KEY (idUsuario)
+    );
+
+CREATE TABLE
+    Espuchifai.Album(
+        nombre VARCHAR(45) NOT NULL,
+        lanzamiento DATE NOT NULL,
+        idBanda SMALLINT UNSIGNED NOT NULL,
+        idAlbum MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
+        cantidad MEDIUMINT UNSIGNED NOT NULL,
+        PRIMARY KEY (idAlbum),
+        CONSTRAINT FK_Album_banda FOREIGN KEY (idBanda) REFERENCES Espuchifai.banda (idBanda),
+        CONSTRAINT UQ_Album_nombre UNIQUE (nombre),
+        FULLTEXT (nombre)
+    );
+
+CREATE TABLE
+    Espuchifai.Cancion (
+        nombre VARCHAR(45) NOT NULL,
+        orden TINYINT UNSIGNED NOT NULL,
+        idAlbum MEDIUMINT UNSIGNED NOT NULL,
+        idCancion INT UNSIGNED NOT NULL AUTO_INCREMENT,
+        cantidad MEDIUMINT UNSIGNED NOT NULL,
+        PRIMARY KEY (idCancion),
+        CONSTRAINT FK_Cancion_Album FOREIGN KEY(idAlbum) REFERENCES Espuchifai.Album (idAlbum),
+        CONSTRAINT UQ_Cancion_nombre UNIQUE (nombre),
+        FULLTEXT (nombre)
+    );
+
+CREATE TABLE
+    Espuchifai.Reproduccion(
+        reproduccion DATETIME NOT NULL,
+        idCancion INT UNSIGNED NOT NULL,
+        idUsuario SMALLINT UNSIGNED NOT NULL,
+        PRIMARY KEY (
+            reproduccion,
+            idCancion,
+            idUsuario
+        ),
+        CONSTRAINT FK_Reproduccion_Cancion FOREIGN KEY (idCancion) REFERENCES Espuchifai.Cancion (idCancion),
+        CONSTRAINT FK_Reproduccion_Usuario FOREIGN KEY (idUsuario) REFERENCES Espuchifai.Usuario (idUsuario)
+    );
---
+++ BD/02-TRIGGERS.sql
@@ -0,0 +1,34 @@
+USE Espuchifai ;
+
+SELECT 'Triggers' Estado ;
+
+
+
+/* 1) Cada vez que se inserta una reproducción, se incrementa el contador de reproducciones de la canción en uno.*/
+
+DELIMITER $$
+
+DROP TRIGGER IF EXISTS AftInsReproduccion $$
+CREATE TRIGGER AftInsReproduccion AFTER
+INSERT ON Reproduccion FOR EACH ROW BEGIN
+
+    UPDATE Cancion
+    SET cantidad = cantidad + 1
+    WHERE idCancion = new.idCancion;
+
+END $$
+
+-- 2) Cada vez que se actualiza el contador de la caanción en N reproducciones, se incrementa el contador del álbum también en N.
+
+DELIMITER $$
+
+DROP TRIGGER IF EXISTS AftUpdReproduccion $$
+CREATE TRIGGER AftUpdReproduccion AFTER
+UPDATE ON Cancion FOR EACH ROW BEGIN IF (NEW.cantidad > OLD.cantidad) THEN
+UPDATE Album
+SET cantidad = cantidad + 1
+WHERE idAlbum = new.idAlbum;
+
+END IF;
+
+END $$ 
