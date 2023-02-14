1cb506bc75ca7d7ecdc2b67c98482ed087ae4e0d
arminPK <armin.mercadoet12d1@gmail.com>
2023-02-14 20:10:11 -0300

    Update 02-TRIGGERS.sql

--- BD/02-TRIGGERS.sql
+++ BD/02-TRIGGERS.sql
@@ -4,13 +4,12 @@ USE Espuchifai ;
 
 DELIMITER $$
 DROP TRIGGER IF EXISTS AftInsReproduccion $$
-CREATE TRIGGER AftInsReproduccion AFTER
-INSERT ON Reproduccion FOR EACH ROW BEGIN
-
+CREATE TRIGGER AftInsReproduccion BEFORE INSERT ON Reproduccion 
+FOR EACH ROW 
+BEGIN
     UPDATE Cancion
     SET cantidad = cantidad + 1
     WHERE idCancion = new.idCancion;
-
 END $$
 
 -- 2) Cada vez que se actualiza el contador de la caanción en N reproducciones, se incrementa el contador del álbum también en N.
@@ -18,12 +17,12 @@ END $$
 DELIMITER $$
 
 DROP TRIGGER IF EXISTS AftUpdReproduccion $$
-CREATE TRIGGER AftUpdReproduccion AFTER
-UPDATE ON Cancion FOR EACH ROW BEGIN IF (NEW.cantidad > OLD.cantidad) THEN
-UPDATE Album
-SET cantidad = cantidad + 1
-WHERE idAlbum = new.idAlbum;
-
-END IF;
-
+CREATE TRIGGER AftUpdReproduccion BEFORE UPDATE ON Cancion 
+FOR EACH ROW 
+BEGIN 
+    IF (NEW.cantidad > OLD.cantidad) THEN
+    UPDATE Album
+    SET cantidad = cantidad + 1
+    WHERE idAlbum = new.idAlbum;
+    END IF;
 END $$ 
