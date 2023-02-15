1f4e52233586e188078f16755bc35b5b9dac90e0
arminPK <armin.mercadoet12d1@gmail.com>
2023-02-15 11:46:36 -0300

    actualizado

---
+++ Espuchifai.Core/Album.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Espuchifai.Core
+{
+    public class Album
+    {
+        public int idbanda { get; set; }
+        
+    }
+}
--- Espuchifai.Core/banda.cs
+++ Espuchifai.Core/banda.cs
@@ -1,5 +1,20 @@
-namespace Espuchifai.Core;
-public class Banda
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Espuchifai.Core
 {
-    
+    public class Banda
+    {
+        public int idBanda;
+        public string Nombre;
+        public int Fundacion;
+        public Banda(int idbanda, string nombre, int fundacion)
+        {
+            this.idBanda=idbanda;
+            this.Nombre=nombre;
+            this.Fundacion=fundacion;
+        }
+    }
 }
