3c72ade12612d99f5661968ecfcbb641295dce98
Gonzaloasd <124898516+Gonzaloasd@users.noreply.github.com>
2024-08-15 04:06:16 -0300

    Update 04Ussers.sql

--- BD/04Ussers.sql
+++ BD/04Ussers.sql
@@ -1,20 +1,27 @@
 --Administrador
 --Desde cualquier lado puede ver todas las tablas.
 --Desde la terminal donde corre el sistema puede agregar y modificar bandas, álbumes y canciones.
-CREATE USER IF NOT EXISTS Administrador@% IDENTIFIED BY '1111';
-GRANT SELECT ON Espuchifai.* TO Administrador;
-GRANT INSERT,UPDATE ON Espuchifai.Bandas TO Administrador;
-GRANT INSERT,UPDATE ON Espuchifai.Albunes TO Administrador;
-GRANT INSERT,UPDATE ON Espuchifai.Canciones TO Administrador;
+
+USE Espuchifai;
+DROP USER IF EXISTS 'Administrador'@'%';
+CREATE USER 'Administrador'@'%' IDENTIFIED BY 'passAdministrador';
+GRANT SELECT ON Espuchifai.* TO 'Administrador'@'%';
+DROP USER IF EXISTS 'Administrador'@'LocalHost';
+CREATE USER 'Administrador'@'LocalHost' IDENTIFIED BY 'passAdministrador';
+GRANT INSERT, UPDATE ON Espuchifai.Banda TO 'Administrador'@'LocalHost';
+GRANT INSERT, UPDATE ON Espuchifai.Album TO 'Administrador'@'LocalHost';
+GRANT INSERT, UPDATE ON Espuchifai.Cancion TO 'Administrador'@'LocalHost';
 --Banda: desde cualquier lado puede dar de alta álbumes, canciones y ver la tabla reproducciones.
-CREATE USER IF NOT EXISTS Banda@% IDENTIFIED BY '2222';
-GRANT INSERT,SELECT ON Espuchifai.Banda TO Banda;
-GRANT INSERT,SELECT ON Espuchifai.Album TO Banda;
-GRANT INSERT,SELECT ON Espuchifai.Cancion TO Banda;
+DROP USER IF EXISTS 'Banda'@'%';
+CREATE USER 'Banda'@'&' IDENTIFIED BY 'passBanda';
+GRANT INSERT ON Espuchifai.Album TO 'Banda'@'&';
+GRANT INSERT ON Espuchifai.Cancion TO 'Banda'@'&';
+GRANT SELECT ON Espuchifai.Repreoduccion TO 'Banda'@'&';
 --Cliente: desde cualquier lado puede ver su información personal, información de las bandas y álbumes. 
 --Con respecto a las reproducciones puede verlas, agregar y eliminar.
-CREATE USER IF NOT EXISTS Cliente@% IDENTIFIED BY '3333';
-GRANT SELECT ON Espuchifai.Cliente TO Cliente;
-GRANT SELECT ON Espuchifai.BandaTO Cliente;
-GRANT SELECT ON Espuchifai.Album TO Cliente;
-GRANT INSEERT,SELECT,DELETE ON Espuchifai.Reproduccion TO Cliente;
+DROP USER IF EXISTS 'Cliente'@'%';
+CREATE USER 'Cliente'@'%' IDENTIFIED BY 'passCliente';
+GRANT SELECT ON Espuchifai.Cliente TO 'Cliente'@'%';
+GRANT SELECT ON Espuchifai.Banda TO 'Cliente'@'%';
+GRANT SELECT ON Espuchifai.Album TO 'Cliente'@'%';
+GRANT INSERT,SELECT,DELETE ON Espuchifai.Reproduccion TO 'Cliente'@'%';
