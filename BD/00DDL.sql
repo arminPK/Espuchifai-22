d68b078df26f00cc0044a01703c6d513c770fd6b
Gonzaloasd <124898516+Gonzaloasd@users.noreply.github.com>
2024-08-14 22:08:48 -0300

    Update 00DDL.sql

--- BD/00DDL.sql
+++ BD/00DDL.sql
@@ -18,55 +18,46 @@ CREATE TABLE Cliente
 
 CREATE TABLE Reproduccion
 (
+    idreproduccion INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    idcliente INT NOT NULL,
+    idcancion TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    momreproduccion DATETIME NOT NULL,
+    PRIMARY KEY (idreproduccion)
+    CONSTRAINT fk_Reproduccion_idcliente FOREIGN KEY(idcliente) REFERENCES Cliente (idcliente),
+    CONSTRAINT fk_Reproduccion_idcancion FOREIGN KEY(idcancion) REFERENCES Cancion (idcancion)
+);
+
+CREATE TABLE Cancion
+(
+    idcancion TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
     nombre VARCHAR(45) NOT NULL,
+    numorden INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    idalbum TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (idcancion),
+    CONSTRAINT fk_Cancion_idalbum FOREIGN KEY(idalbum) REFERENCES Album (idalbum)
+);
+
+CREATE TABLE Album
+(
+    idalbum TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(45) NOT NULL,
+    lanzamiento DATE NOT NULL,
+    idbanda SAMALLINT UNSIGNED NOT NULL,
+    PRIMARY KEY (idalbum),
+    CONSTRAINT fk_Album_idbanda FOREIGN KEY(idbanda) REFERENCES Bandaa (idbanda)
+);
+
+CREATE TABLE Banda
+(
+    idbanda SMALLINT UNSIGNED NOT NULL,
+    nombre VARCHAR(45) NOT NULL,
+    fundacion YEAR NOT NULL,
+    PRIMARY KEY (idbanda)
+);
+(
+    idreproduccion INT UNSIGNED NOT NULL AUTO_INCREMENT,
     fundacion YEAR NOT NULL,
     idBanda SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
     PRIMARY KEY(idBanda),
     FULLTEXT (nombre)
-);
-
-CREATE TABLE Espuchifai.Album
-(
-    nombre VARCHAR(45) NOT NULL,
-    lanzamiento DATE NOT NULL,
-    idBanda SMALLINT UNSIGNED NOT NULL,
-    idAlbum SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    cantidad MEDIUMINT UNSIGNED NOT NULL,
-    PRIMARY KEY (idAlbum),
-    CONSTRAINT FK_Album_banda FOREIGN KEY (idBanda) REFERENCES Espuchifai.banda (idBanda),
-    CONSTRAINT UQ_Album_nombre UNIQUE (nombre),
-    FULLTEXT (nombre)
-);
-
-CREATE TABLE Espuchifai.Cancion 
-(
-    nombre VARCHAR(45) NOT NULL,
-    posicion TINYINT UNSIGNED NOT NULL,
-    idAlbum SMALLINT UNSIGNED NOT NULL,
-    idCancion INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    cantidad MEDIUMINT UNSIGNED NOT NULL,
-    PRIMARY KEY (idCancion),
-    CONSTRAINT FK_Cancion_Album FOREIGN KEY(idAlbum) REFERENCES Espuchifai.Album (idAlbum),
-    CONSTRAINT UQ_Cancion_nombre UNIQUE (nombre),
-    FULLTEXT (nombre)
-);
-
-CREATE TABLE Espuchifai.Reproduccion
-(
-    reproduccion DATETIME NOT NULL,
-    idCancion INT UNSIGNED NOT NULL,
-    idCliente int UNSIGNED NOT NULL,
-    PRIMARY KEY ( reproduccion, idCancion, idCliente),
-    CONSTRAINT FK_Reproduccion_Cancion FOREIGN KEY (idCancion) REFERENCES Espuchifai.Cancion (idCancion),
-    CONSTRAINT FK_Reproduccion_Cliente FOREIGN KEY (idCliente) REFERENCES Espuchifai.Cliente (idCliente)
-);
-
-CREATE TABLE Espuchifai.Cliente
-(
-    nombre VARCHAR (45) NOT NULL,
-    apellido VARCHAR (45) NOT NULL,
-    email VARCHAR (60) NOT NULL,
-    contrasenia CHAR (64) NOT NULL,
-    idCliente int UNSIGNED NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (idCliente)
 );
